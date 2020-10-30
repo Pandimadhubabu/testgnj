d28f4afad64bf59a1fcaf3507384189a08e0f0a8
Madhu Babu <17622671+Pandimadhubabu@users.noreply.github.com>
2020-10-30 15:46:13 +0530

    Create navigation.ts

---
+++ pages/api/navigation.ts
@@ -0,0 +1,17 @@
+[
+   {
+      "id":1,
+      "title":"About",
+      "slug":"/about"
+   },
+   {
+      "id":1,
+      "title":"Contact",
+      "slug":"/contact"
+   },
+   {
+      "id":3,
+      "title":"Blog",
+      "slug":"/blog"
+   }
+]
