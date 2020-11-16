449adfdbf19e83677056b8359320bf506bfe9ee3
Madhu Babu <17622671+Pandimadhubabu@users.noreply.github.com>
2020-11-16 21:28:12 +0530

    Update [lang].ts

--- pages/api/covid/[lang].ts
+++ pages/api/covid/[lang].ts
@@ -4,12 +4,12 @@ export default async (req, res) => {
   const {lang} = req.query
   const news_1: any = await googleNewsAPI.getNews(
     googleNewsAPI.SEARCH,
-    'corona virus',' ${lang} '
+    '${lang}','te'
   );
 
   const news_2: any = await googleNewsAPI.getNews(
     googleNewsAPI.SEARCH,
-    'pandemic',' {lang} '
+    '{lang}','te'
   );
 
   const totalNews = news_1.items.concat(news_2.items);
