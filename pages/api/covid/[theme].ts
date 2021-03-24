067ebb04cb977feab7ac186e4bb286cd0d35a6d5
Madhu Babu <17622671+Pandimadhubabu@users.noreply.github.com>
2021-03-24 13:57:35 +0530

    Update [theme].ts

--- pages/api/covid/[theme].ts
+++ pages/api/covid/[theme].ts
@@ -1,16 +1,16 @@
 import googleNewsAPI from 'google-news-json';
 
 export default async (request, res) => {
-  const { theme } = request.query
+  const {theme} = request.query
   
   const news_1: any = await googleNewsAPI.getNews(
     googleNewsAPI.SEARCH,
-    '{ theme }','en-IN'
+    '${theme}','en-IN'
   );
 
   const news_2: any = await googleNewsAPI.getNews(
     googleNewsAPI.SEARCH,
-    '{ theme }','en-IN'
+    '${theme}','en-IN'
   );
   
   
